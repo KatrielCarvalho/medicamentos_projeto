27299acfc0cd20c6925de65013afc5d6b74c53b1
Katriel <katrielmilena@gmail.com>
2026-05-26 09:54:24 -0300

    Comeco do codigo do projeto

--- medicamentos_nucleo.cpp
+++ medicamentos_nucleo.cpp
@@ -4,8 +4,41 @@
 
 using namespace std;
 
+struct Medicamento{
+
+    int id;
+    string nome_comercial;
+    string principio_ativo;
+    float dosagem;
+    string laboratorio;
+
+};
+
 int main(){	
     ifstream arquivo_medicamentos("medicamentos.csv");
+    
+    char letra;
+    string linha;
 
+    int num_registros = 0;
+
+    getline(arquivo_medicamentos, linha);
+    arquivo_medicamentos >> num_registros;
+    arquivo_medicamentos.ignore();
+    
+    Medicamento vet_medicamentos[num_registros];
+
+    arquivo_medicamentos >> vet_medicamentos[0].id;
+    
+    arquivo_medicamentos >> letra;
+
+    getline(arquivo_medicamentos, vet_medicamentos[0].nome_comercial, ',');
+
+    arquivo_medicamentos >> letra;
+
+    getline(arquivo_medicamentos, vet_medicamentos[0].principio_ativo, ',');
+
+
+    arquivo_medicamentos.close();
     return 0;
 }
