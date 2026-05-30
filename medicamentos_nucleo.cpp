8c998bf6e46aeb95a9dab072bc28b3e31ab49a1c
Katriel <katrielmilena@gmail.com>
2026-05-30 18:11:12 -0300

    Implementação básica de criação de um vetor que percorre os elementos de um arquivo .csv, com o tamanho do vetor sendo passado previamente.

--- medicamentos_nucleo.cpp
+++ medicamentos_nucleo.cpp
@@ -13,33 +13,46 @@ struct Medicamento{
     string laboratorio;
 
 };
-cout<<"teste"<endl;
 
-int main(){	
+int main(){
+
     ifstream arquivo_medicamentos("medicamentos.csv");
-    
-    char letra;
-    string linha;
 
-    int num_registros = 0;
+    if (!arquivo_medicamentos){
+      cout << "Não foi possível abrir o arquivo" << endl;
+    }
+    else{
+      char letra;
+      string linha;
 
-    getline(arquivo_medicamentos, linha);
-    arquivo_medicamentos >> num_registros;
-    arquivo_medicamentos.ignore();
-    
-    Medicamento vet_medicamentos[num_registros];
+      int num_registros = 0;
 
-    arquivo_medicamentos >> vet_medicamentos[0].id;
-    
-    arquivo_medicamentos >> letra;
+      getline(arquivo_medicamentos, linha);
+      arquivo_medicamentos >> num_registros;
+      arquivo_medicamentos.ignore();
+      
+      Medicamento vet_medicamentos[num_registros];
 
-    getline(arquivo_medicamentos, vet_medicamentos[0].nome_comercial, ',');
+      for(int i=0; i<num_registros; i++){
+      
+        arquivo_medicamentos >> vet_medicamentos[i].id;
+        
+        arquivo_medicamentos >> letra;
 
-    arquivo_medicamentos >> letra;
+        getline(arquivo_medicamentos, vet_medicamentos[i].nome_comercial, ',');
 
-    getline(arquivo_medicamentos, vet_medicamentos[0].principio_ativo, ',');
+        getline(arquivo_medicamentos, vet_medicamentos[i].principio_ativo, ',');
 
+        arquivo_medicamentos >> vet_medicamentos[i].dosagem;
 
+        arquivo_medicamentos >> letra;
+
+        getline(arquivo_medicamentos, vet_medicamentos[i].laboratorio);
+
+        cout << 'A';
+      
+      }
+    }   
     arquivo_medicamentos.close();
     return 0;
 }
